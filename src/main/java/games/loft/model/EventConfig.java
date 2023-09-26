d123f61fff7801fb109622a305a63d91b2a5c2d5
MemoryShadow <MemoryShadow@outlook.com>
2023-09-26 10:23:16 +0800

    perf: 缓存上一次找到的选区进行优先匹配

--- src/main/java/games/loft/model/EventConfig.java
+++ src/main/java/games/loft/model/EventConfig.java
@@ -1,7 +1,7 @@
 /*
  * @Date: 2023-09-21 18:46:29
  * @LastEditors: MemoryShadow
- * @LastEditTime: 2023-09-25 22:23:10
+ * @LastEditTime: 2023-09-26 10:22:36
  * @Description: 一份对当前事件的配置
  * Copyright (c) 2023 by MemoryShadow@outlook.com, All Rights Reserved.
  */
@@ -18,6 +18,7 @@ import lombok.Setter;
 public class EventConfig {
     boolean Enable = true;
     boolean Hurt = true;
+    Cluster CacheConfig = null;
     int X;
     int Y;
     int Z;
@@ -34,13 +35,21 @@ public class EventConfig {
         this.Y = (int) (location.getY() + .5);
         this.Z = (int) (location.getZ() + .5);
         // 加载配置信息并初始化基础信息
-        Cluster config = AntiExplosionConfig.GetAntiExplosionConfig().valueOf(this.X, this.Y, this.Z);
+        Cluster config;
+        // 如果有缓存, 就直接使用缓存检测, 如果命中缓存就不需要再次搜索
+        if (this.CacheConfig != null && this.CacheConfig.isInside(this.X, this.Y, this.Z)) {
+            config = this.CacheConfig;
+        } else {
+            config = AntiExplosionConfig.GetAntiExplosionConfig().valueOf(this.X, this.Y, this.Z);
+        }
         if (config == null) {
             // 如果没有搜索到, 就直接使用全局配置
             AntiExplosionConfig temp = AntiExplosionConfig.GetAntiExplosionConfig();
             this.Enable = temp.isEnable();
             this.Hurt = temp.isHurt();
         } else {
+            // 如果搜索到了, 就缓存配置信息以便优化下一次搜索
+            this.CacheConfig = config;
             this.Enable = config.isEnable();
             this.Hurt = config.isHurt();
         }
